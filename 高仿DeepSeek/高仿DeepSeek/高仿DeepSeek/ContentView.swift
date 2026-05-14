d9c244af9248c777ccaca930cd45a38c9e8eb892
williamasunnyboy-coder <williamasunnyboy@gmail.com>
2026-05-14 18:52:52 +0800

    Add files via upload

---
+++ 高仿DeepSeek/高仿DeepSeek/高仿DeepSeek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  高仿DeepSeek
+//
+//  Created by 宋阳 on 2026/5/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 高仿DeepSeek/高仿DeepSeek/高仿DeepSeek/__DeepSeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  __DeepSeekApp.swift
+//  高仿DeepSeek
+//
+//  Created by 宋阳 on 2026/5/14.
+//
+
+import SwiftUI
+
+@main
+struct __DeepSeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
